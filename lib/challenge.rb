6af8a686ac40243f366f8b87e2f27b6348a0dc4c
MishaHerscu <mishaherscu@gmail.com>
2016-07-22 09:31:42 -0400

    response, still doesn't work

--- lib/challenge.rb
+++ lib/challenge.rb
@@ -1,3 +1,19 @@
+def empty_check(expr)
+  if expr == ''
+    false
+  else
+    true
+  end
+end
+
 def calc(expr)
-  # Your code here
+  return 0 if empty_check(expr) == false
+  inputs = expr.split(' ')
+  operation = inputs[-1]
+  operating_string = ''
+  inputs[0...-1].each do |x|
+    operating_string += inputs[x]
+    operating_string += operation
+  end
+  eval(operating_string)
 end
